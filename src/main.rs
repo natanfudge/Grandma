e6325dec46cfa947bcc6b8bed35d9a6fbc6b3c44
natanfudge <natan.lifsiz@gmail.com>
2019-10-01 01:31:20 +0300

    try to do it dynamically

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::{read_dir, File};
 use walkdir::{WalkDir, DirEntry};
 use git2::{Repository, Oid};
 use git2::build::CheckoutBuilder;
-use crate::git::{GitExt, YARN_REPO};
+use crate::git::{GitExt, YARN_REPO, YarnRepo};
 
 
 #[macro_use]
@@ -37,10 +37,15 @@ mod git;
 
 fn main() {
     println!("Program started!");
+    println!("Cloning yarn...");
+    let new_repo = YarnRepo::get_or_clone_yarn(get_resource("test_repo"));
+
     println!("Finding directories in resources folder:");
     for x in WalkDir::new(get_resource("")).into_iter().filter_map(Result::ok) {
         println!("File: {:?}", x.into_path());
     }
+
+
 //    let repository = Repository::open(get_resource("Fudge"))
 //        .expect("Could not open yarn repository");
 
@@ -48,7 +53,7 @@ fn main() {
 
 //    github::send_pr();
 //    println!("Parsing mappings...");
-    let mappings_dir = get_resource("Fudge/mappings");
+    let mappings_dir = get_resource("test_repo/mappings");
 //
     let mappings: Vec<ClassMapping> = WalkDir::new(mappings_dir)
         .into_iter()
